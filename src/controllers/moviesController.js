cc43e4379d744e83797c7f1683d8937dbbf319e2
Richona <105828822+Richona@users.noreply.github.com>
2022-09-23 21:51:39 -0300

    movies database completed

---
+++ src/controllers/moviesController.js
@@ -0,0 +1,29 @@
+const db = require("../database/models");
+
+module.exports = {
+  list: (req, res) => {
+    db.Movie.findAll()
+      .then((movies) => res.render("moviesList", { movies }))
+      .catch((error) => console.log(error));
+  },
+  new: (req, res) => {
+    db.Movie.findAll({
+      order: [["release_date", "DESC"]],
+    })
+      .then((movies) => res.render("newestMovies", { movies }))
+      .catch((error) => console.log(error));
+  },
+  recommended: (req, res) => {
+    db.Movie.findAll({
+      order: [["release_date", "DESC"]],
+      limit: 5
+    })
+      .then((movies) => res.render("recommendedMovies", { movies }))
+      .catch((error) => console.log(error));
+  },
+  detail: (req, res) => {
+    db.Movie.findByPk(req.params.id)
+      .then((movie) => res.render("moviesDetail", { movie }))
+      .catch((error) => console.log(error));
+  },
+};
